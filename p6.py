f6e80e943a94f37fbefb12c3ca076f5434464c91
RAR-040737 <115770777+RAR-040737@users.noreply.github.com>
2022-10-14 10:49:21 +0600

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,13 @@
+def clean_string(text):
+    output = ""
+    for c in text:
+        if (c >= 'A' and c <= 'Z') or (c >= 'a' and c <= 'z'):
+            output += c
+    # print(output)
+    return output
+
+
+s = "P::::::,,,,,h;;;;i,,,t--r;,:o..N"
+
+output = clean_string(s)
+print(output)
---
+++ p8.py
@@ -0,0 +1,20 @@
+def test_script():
+    text = "my Name Is atiqur RAhman"
+
+    def make_upper(text):
+        u = text.upper()
+        print(u)
+    make_upper(text)
+
+    def make_capital(text):
+        c = text.capitalize()
+        print(c)
+    make_capital(text)
+
+    def make_lower(text):
+        l = text.lower()
+        print(l)
+    make_lower(text)
+
+
+test_script()
